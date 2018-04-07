c4aa46d6374088ae947d0ab5070a4a18db1a22f1
Joshua <jls232523@email.arizona.edu>
2018-04-07 14:23:41 -0700

    charecter

---
+++ SucculentAdventure/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour
+{
+    Transform player;
+    PlayerHealth playerHealth;
+    EnemyHealth enemyHealth;
+    UnityEngine.AI.NavMeshAgent nav;
+	//ScoreManager scoreManager;
+
+
+    void Awake ()
+    {
+        player = GameObject.FindGameObjectWithTag ("Player").transform;
+        playerHealth = player.GetComponent <PlayerHealth> ();
+        enemyHealth = GetComponent <EnemyHealth> ();
+        nav = GetComponent <UnityEngine.AI.NavMeshAgent> ();
+    }
+
+
+    void Update ()
+    {
+		if (enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0) {
+			nav.SetDestination (player.position);
+		}
+			/*if (ScoreManager.score >= 500) {
+				nav.speed = 5f;
+				nav.angularSpeed = 240f;
+			}
+			if (ScoreManager.score >= 1000) {
+				nav.speed = 10f;
+				nav.angularSpeed = 360f;
+			}
+			if (ScoreManager.score >= 1500) {
+				nav.speed = 12f;
+				nav.angularSpeed = 360f;
+			}
+		}*/
+        else
+        {
+            nav.enabled = false;
+        }
+    }
+}
+
---
+++ SucculentAdventure/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public Rigidbody rb;
+	public float speed;
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		speed = 100000;
+		
+	}
+	void Update(){
+	}
+
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+		rb.AddForce (movement*speed);
+
+	}
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.CompareTag("Pick Up")){
+			other.gameObject.SetActive(false);
+		}
+	}
+}
